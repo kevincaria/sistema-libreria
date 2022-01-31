e169d7407c1c074eeca9b3ced4e5236f90e4540b
Kevin Caria <kevindcaria@gmail.com>
2022-01-31 20:59:44 -0300

    encriptador de password

--- src/main/java/libreria/demo/util/EncriptarPassword.java
+++ src/main/java/libreria/demo/util/EncriptarPassword.java
@@ -1,5 +1,17 @@
 package libreria.demo.util;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class EncriptarPassword {
+    public static void main(String[] args) {
+        String password = "123";
+        System.out.println("Password: " + password);
+        System.out.println("Password encriptado: " + encriptarPassword(password));
+
+    }
     
+    public static String encriptarPassword(String password){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(password);
+    }
 }
